80a306bff8e1c13d38b810a53d5f149f49857e3b
maxsynnott <maxryansynnott@gmail.com>
2020-07-13 16:05:37 +0200

    Hello World

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.json({
+		message: 'Hello World!'
+	});
+});
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+	console.log("App listening on port " + port);
+});
